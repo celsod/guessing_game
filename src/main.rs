da308432a241a8363f368f7505d3300b3d07ac60
celsod <cdesouzajr@gmail.com>
2024-05-04 15:06:03 -0500

    This is the completed guessing game tutorial from the Rust tutorial

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,31 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=10);
     //This line above creates a secret number variable that is random between 1 and 10
 
-    println!("The secret number is: {}", secret_number); //This is similar to the fstring in python
+    //println!("The secret number is: {}", secret_number); //This is similar to the fstring in python
+    //uncomment the line above it you want to see the secret number
 
-    println!("Please input your guess:");
+    loop {
+        println!("Please input your guess:");
 
-    let mut guess = String::new(); //Created a new variable called guess and is able to be changed
+        let mut guess = String::new(); //Created a new variable called guess and is able to be changed
 
-    io::stdin()
-        .read_line(&mut guess) //calls the read_line method for input to the program
-        //the &mut guess is the argument to pass the input into
-        .expect("Failed to read the line");
+        io::stdin()
+            .read_line(&mut guess) //calls the read_line method for input to the program
+            //the &mut guess is the argument to pass the input into
+            .expect("Failed to read the line"); //error code if something goes wrong
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() { //trim removes whitespace around the input and parse converts the string to u32
+            Ok(num) => num, //if guess is a number move to the next step in the program
+            Err(_) => continue, //if guess is not a number, repeat the loop
+        };
+        println!("You guessed: {}", guess);
     
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("Correct!"),
-        Ordering::Greater => println!("Too large!"),
+        match guess.cmp(&secret_number){ //compare function between guess and secret number
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => { //function to end the program when the correct number is selected
+                println!("Correct!");
+                break;}
+            Ordering::Greater => println!("Too large!"),
+        }
     }
 }
